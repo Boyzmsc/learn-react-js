d0be3fc11343057686b91cfd7163365b579de04a
Boyzmsc <boyzmsc@naver.com>
2021-02-08 02:48:41 +0900

    feat: Implement pageChange

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
     super(props);
     this.state = {
       mode: 'read',
+      selected_content_id: 3,
       subject: { title: "WEB", sub: "World Wide Web!" },
       welcome: { title: "Welcome", desc: "Hello, React!!" },
       contents: [
@@ -21,25 +22,42 @@ class App extends Component {
   }
   render() {
     var _title, _desc = null;
+
     if (this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+
     } else if (this.state.mode === 'read') {
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      var i = 0;
+      while (i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i += 1;
+      }
     }
     return (
       <div className="App">
         <Subject
           title={this.state.subject.title}
           sub={this.state.subject.sub}
-          onChangePage = {function(){
+          onChangePage={function () {
             this.setState({
               mode: 'welcome'
             })
           }.bind(this)}>
         </Subject>
-        <TOC data={this.state.contents}></TOC>
+        <TOC
+          onChangePage={function (id) {
+            this.setState({
+              mode: 'read',
+              selected_content_id: Number(id)
+            });
+          }.bind(this)}
+          data={this.state.contents}></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
     );
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -7,9 +7,16 @@ class TOC extends Component {
         var data = this.props.data;
         var i = 0;
         while (i < data.length) {
-            lists.push(<li key = {data[i].id}>
-            <a href={"/content/" + data[i].id}>{data[i].title}</a>
-            </li>);
+            lists.push(
+                <li key={data[i].id}>
+                    <a
+                        href={"/content/" + data[i].id}
+                        onClick={function (id, e) {
+                            e.preventDefault();
+                            this.props.onChangePage(id);
+                        }.bind(this, data[i].id)}>
+                        {data[i].title}</a>
+                </li>);
             i = i + 1;
         }
         return (
