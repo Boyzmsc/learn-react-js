0cc8f032c02e59e385282ad4a698c9e3d956c7e1
Boyzmsc <boyzmsc@naver.com>
2021-02-08 00:24:49 +0900

    feat: Separate Components

--- src/App.js
+++ src/App.js
@@ -1,50 +1,17 @@
 import './App.css';
 import { Component } from 'react';
 
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>WEB</h1>
-        World Wide Web!
-      </header>
-    );
-  }
-}
-
-// TOC : Table Of Contents
-class TOC extends Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
-        </ul>
-      </nav>
-    );
-  }
-}
-
-class Content extends Component {
-  render() {
-    return (
-      <article>
-        <h2>HTML</h2>
-        HTML is HyperText Markup Language.
-      </article>
-    );
-  }
-}
+import Subject from './components/Subject';
+import TOC from './components/TOC';
+import Content from './components/Content';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Subject></Subject>
+        <Subject title="WEB" sub="World Wide Web!"></Subject>
         <TOC></TOC>
-        <Content></Content>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
   }
