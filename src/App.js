f07a676772cf9caac39f30b8b2177f06bf5c112a
Boyzmsc <boyzmsc@naver.com>
2021-02-08 01:10:30 +0900

    feat: Refactoring components using state

--- src/App.js
+++ src/App.js
@@ -6,11 +6,24 @@ import TOC from './components/TOC';
 import Content from './components/Content';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: "WEB", sub: "World Wide Web!" },
+      contents: [
+        {id:1, title: 'HTML', desc:'HTML is for information'},
+        {id:2, title: 'CSS', desc:'CSS is for design'},
+        {id:3, title: 'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="World Wide Web!"></Subject>
-        <TOC></TOC>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}></Subject>
+        <TOC data = {this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -3,12 +3,19 @@ import { Component } from 'react';
 // TOC : Table Of Contents
 class TOC extends Component {
     render() {
+        var lists = [];
+        var data = this.props.data;
+        var i = 0;
+        while (i < data.length) {
+            lists.push(<li key = {data[i].id}>
+            <a href={"/content/" + data[i].id}>{data[i].title}</a>
+            </li>);
+            i = i + 1;
+        }
         return (
             <nav>
                 <ul>
-                    <li><a href="1.html">HTML</a></li>
-                    <li><a href="2.html">CSS</a></li>
-                    <li><a href="3.html">JavaScript</a></li>
+                    {lists}
                 </ul>
             </nav>
         );
